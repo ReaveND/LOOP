a058e67d61a4791a59718c6e2cb2fe8c6db81909
ReaveND <reaverrupak@gmail.com>
2026-07-27 23:06:05 +0530

    feat: implement database seeding script and workspace test API endpoint

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,7 +2,18 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 
 export async function GET() {
-  const users = await prisma.user.findMany();
+  const workspace = await prisma.workspace.findMany({
+  include: {
+    users: {
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+      },
+    },
+  },
+});
 
-  return NextResponse.json(users);
+  return NextResponse.json(workspace);
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,34 @@
 import { prisma } from "../lib/db";
+import { UserRole } from "../lib/generated/prisma/client";
 
 async function main() {
-  console.log("Seeding database...");
+  console.log("🌱 Seeding database...");
 
   const workspace = await prisma.workspace.create({
     data: {
       name: "Demo Workspace",
+
+      users: {
+        create: {
+          name: "Rupak",
+          email: "reaverrupak@gmail.com",
+          passwordHash: "password123", // We'll hash this later
+          role: UserRole.ADMIN,
+        },
+      },
+    },
+    include: {
+      users: true,
     },
   });
 
-  console.log("✅ Workspace created:", workspace);
+  console.log("✅ Workspace created successfully!");
+  console.log(workspace);
 }
 
 main()
   .catch((error) => {
-    console.error(error);
+    console.error("❌ Seed failed:", error);
     process.exit(1);
   })
   .finally(async () => {
