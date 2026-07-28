5859fd022a027032362f7d3b12775407c09a8676
ReaveND <reaverrupak@gmail.com>
2026-07-28 11:47:46 +0530

    feat: add test API route to fetch user by email

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,19 +1,35 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 
-export async function GET() {
-  const workspace = await prisma.workspace.findMany({
-  include: {
-    users: {
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        role: true,
-      },
-    },
-  },
-});
+// export async function GET() {
+//   const workspace = await prisma.workspace.findMany({
+//   include: {
+//     users: {
+//       select: {
+//         id: true,
+//         name: true,
+//         email: true,
+//         role: true,
+//       },
+//     },
+//   },
+// });
 
-  return NextResponse.json(workspace);
+//   return NextResponse.json(workspace);
+// }
+
+export async function GET() {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: "reaverrupak@gmail.com",
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+    },
+  });
+
+  return NextResponse.json(user);
 }
